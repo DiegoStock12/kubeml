bdc198de99dbe839a0bbcf025c53ed3e39b78d67
diegostock12 <diego.albo.martinez@gmail.com>
2020-11-16 12:15:47 +0100

    Got torch to work inside the containers

--- example.py
+++ example.py
@@ -1,2 +1,11 @@
+from flask import current_app
+import torch
+
 def main():
-	return help('modules')
+	a = torch.Tensor([1,2,3,4])
+	current_app.logger.info(f'This is a log, {str(a)}')
+	current_app.logger.info(torch.cuda.is_available())
+	# current_app.logger.info(torch.cuda.current_device())
+	return f'Hello I am a function and my result is {str(a)}'
+	
+
