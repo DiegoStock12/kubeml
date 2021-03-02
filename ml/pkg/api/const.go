0a87af0ebdb48f5a4759f371a7df7f7df228a0a4
diegostock12 <diego.albo.martinez@gmail.com>
2021-03-02 13:28:38 +0100

    small fix

--- ml/pkg/api/const.go
+++ ml/pkg/api/const.go
@@ -2,29 +2,26 @@ package api
 
 // Addresses of services
 const (
+	FissionRouterUrl   = "http://router.fission"
 	StorageUrl         = "http://storage.kubeml"
 	SchedulerUrl       = "http://scheduler.kubeml"
 	ParameterServerUrl = "http://parameter-server.kubeml"
 	ControllerUrl      = "http://controller.kubeml"
-)
-
-const (
-	FissionRouterUrl = "http://router.fission"
-	MongoUrl         = "mongodb.kubeml"
-	MongoPort        = 27017
-	RedisUrl         = "redisai.kubeml"
-	RedisPort        = 6379
+	MongoUrl           = "mongodb.kubeml"
+	MongoPort          = 27017
+	RedisUrl           = "redisai.kubeml"
+	RedisPort          = 6379
 )
 
 const DefaultParallelism = 5
 
 // Debug
 const (
-	MongoUrlDebug            = "mongodb://192.168.99.101:30933"
+	MongoUrlDebug            = "mongodb://192.168.99.101:30074"
 	StorageAddressDebug      = "http://192.168.99.102:9090"
 	FissionRouterUrlDebug    = "http://192.168.99.101:32422"
 	RedisAddressDebug        = "192.168.99.101"
-	RedisPortDebug           = 31618
+	RedisPortDebug           = 30358
 	DebugParallelism         = 2
 	SchedulerPortDebug       = 10200
 	ParameterServerPortDebug = 10300
